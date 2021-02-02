90a3c99bc9887d79eb9b7811b89b454d67cabb96
Yizi-Yeh <g384357@gmail.com>
2021-02-02 16:17:09 +0800

    登入註冊
    登入註冊

--- src/components/members/Log.vue
+++ src/components/members/Log.vue
@@ -1,36 +1,38 @@
 <template>
 <div>
-  <div class="sidenav">
-            <div class="login-main-text">
-            <h2> Nice to meet you!<br>Feel free to join MT10x10.</h2>
+<div>
+<div class="container mt-5">
+  <div class="row mt-5">
+    <div class="col-12 mt-5 d-flex flex-column align-items-center">
+            <h2 text-center> Feel free to join MT10x10</h2>
             <p>Are you ready to have a fantastic hiking with us?</p>
- <router-link to="/memberReg">
+ <form class="form-signin rounded border d-flex flex-column align-items-center" >
+  <img class="mb-3 mt-5"  src="https://mt10x10.files.wordpress.com/2020/07/cropped-e7b6b2e7ab99e6a899e9a18c-e6a899e8aa8c-4.jpg" alt="" >
+  <h4 class="mb-2">MT會員登入</h4>
+  <label for="inputEmail" class="sr-only mb-2">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" v-model="account" :state="accountState" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password" :state="passwordState" required >
+  <div class="checkbox mb-3">
+  </div>
+  <button class="btn btn-lg btn-dark btn-block" type="submit" @click="onSubmit">Signin</button>
+  <button class="btn btn-lg btn-dark btn-block" type="reset" @reset="onReset"> Reset</button>
+  <div class="row">
+    <div class="col-12 mt-4 mr-0">
+   <router-link to="/memberReg">
              <button  class="btn btn-secondary ml-2">註冊</button>
           </router-link>
           <router-link to="/memberLogin">
              <button  class="btn btn-secondary ml-2">登入</button>
           </router-link>
+        </div>
           </div>
-      </div>
-      <div class="main">
-          <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-
-                <form>
-                  <div class="form-group">
-                      <label>Email Address</label>
-                      <input type="text" class="form-control" placeholder="Email Address" v-model="account" :state="accountState">
-                  </div>
-                  <div class="form-group">
-                      <label>Password</label>
-                      <input type="password" class="form-control" placeholder="Password" v-model="password" :state="passwordState">
-                  </div>
-                  <button type="submit" class="btn btn-dark" @click="onSubmit">Submit</button>
-                  <button type="reset" class="btn btn-secondary ml-2" @reset="onReset">Reset</button>
-                </form>
-            </div>
-          </div>
-      </div>
+  <p class="mt-5 mb-3 text-center">&copy; 2021 MT10x10 Cor.</p>
+</form>
+</div>
+</div>
+</div>
+</div>
 </div>
 </template>
 
@@ -99,3 +101,42 @@ export default {
 }
 </script>
 
+
+<style scoped lang="scss">
+html,
+body {
+  height: 100%;
+}
+
+
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
