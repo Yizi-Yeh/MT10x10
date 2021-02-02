49d0db5dd4fb82a89460c75c76bc05e224931bdf
Yizi-Yeh <g384357@gmail.com>
2021-02-02 15:15:23 +0800

    admin
    admin

--- src/components/pages/login.vue
+++ src/components/pages/login.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="container mt-5">
+<div class="container mt-5 ">
   <div class="row mt-5">
     <div class="col-12 mt-5">
- <form class="form-signin" @submit.prevent="signin">
-  <img class="mb-4 mt-5 ml-3"  src="https://mt10x10.files.wordpress.com/2020/07/cropped-e7b6b2e7ab99e6a899e9a18c-e6a899e8aa8c-4.jpg" alt="" >
-  <h1 class="h3 mb-3 font-weight-normal text-center">please sign in</h1>
+ <form class="form-signin rounded border d-flex flex-column align-items-center" @submit.prevent="signin" >
+  <img class="mb-3 mt-5"  src="https://mt10x10.files.wordpress.com/2020/07/cropped-e7b6b2e7ab99e6a899e9a18c-e6a899e8aa8c-4.jpg" alt="" >
+  <h4 class="mb-2">MT管理員登入</h4>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" v-model="user.account" required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
